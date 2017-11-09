f549346115aff66ffa0b6647d3b9cfdff1c0c99a
tjiasan <jason.tjia@gmail.com>
2017-11-09 16:38:19 -0500

    wrote first function

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-var wordnet = require('wordnet');
-var nlp_toolkit = require('nlp-toolkit');
-var Promise = require('bluebird');
+module.exports = () => {
 
+    const wordnet = require('wordnet');
+    const nlp_toolkit = require('nlp-toolkit');
+    const Promise = require('bluebird');
 
-get_synset_type = (word) => {
-    
-    return new Promise((resolve, reject) => {
-        let results = [];
-        wordnet.lookup(word, (err,definitions) => {
-            definitions.forEach(def => {
-                results.push(def.meta.synsetType);
+    const NLP_Classifier = { 
+        GetSynsetType: (word) => {
+            return new Promise((resolve, reject) => {
+                let results = [];
+                wordnet.lookup(word, (err,definitions) => {
+                    definitions.forEach(def => {
+                        results.push(def.meta.synsetType);
+                    });
+                    resolve(results);
+                });
             });
-            resolve(results);
-        });
-    });
+        },
 
-}
+        /**
+         * Create a 1D to classify words
+         *
+         * @param   {Array}       word_list         - word pair that are opposites, must be the same lexical type
+         * @param   {Array}       sentences         - An array of senteces to train the model, must contain word pairs
+         * @return  {obj}                           - A promise of an Office object
+         */
+        CreateModel: (word_list, sentences) =>{
+             
+        }
 
 
-main = () => {
+    }
 
-   return get_synset_type('evil')
-            .then((res) => {
-                console.log(res)
-            })
+    return NLP_Classifier;
+
 }
